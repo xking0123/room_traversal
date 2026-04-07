dc94c1cd69b0f6d90e521ddc813620cff9f4289f
xking0123 <xking012344@gmail.com>
2026-04-07 17:52:58 -0400

    baby steps....

--- Program.cs
+++ Program.cs
@@ -1,2 +1,68 @@
 ﻿//make a game like battle system but have player bounce around between various kinds of rooms rather than battles
 //have 1 room be a combat room, have 1 room be a room where you can heal, have 1 room where you can quit
+
+using System.Net;
+using System.Runtime.CompilerServices;
+
+public static partial class Program
+{
+    public static void Main()
+    {
+        Console.Clear();
+        Console.WriteLine("WELCOME TO THE VOID HOTEL... we hope you enjoy your stay!");
+        Console.WriteLine("here's a master key... explore the rooms as you see fit. just don't get lost in the void now :)");
+
+        //WORK:
+        //put in player stats for battles
+        //make list of (multiple?) battles to randomly generate in the combat room- actions will only be attack, defend, magic
+        //allow recovery room to heal like healing action in battle system
+        //game over when player health reaches 0 (allow them to start over? - reset game function?)
+        //allow game to generate new room after done acting in current room and go through if statments again...? HOW!!!!
+
+        //picking a single random element
+        //using the built-in Random class to make life ez(?)
+        var rooms = new List<string> { "Battle", "Rest", "Freedom" };
+
+        //random object
+        Random rnd = new Random();
+
+        //get random index
+        int index = rnd.Next(rooms.Count);
+
+        //print random room 
+        Console.WriteLine(rooms[index]);
+
+        while (true)
+        {
+            if (rooms[index] == "Freedom")
+            {
+                Console.WriteLine("Do you wish to leave this trap? (Y/N)");
+                string response = Console.ReadLine();
+                if (response == "N" || response == "n")
+                {
+                    Console.WriteLine("yippeee... more CHAAAOOOSSS");
+                    Console.WriteLine(rooms[index]);
+                }
+                if (response == "Y" || response == "y")
+                {
+                    Console.WriteLine("bye... coward...");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("um.... no clue what you said... TO THE VOID WITH YOU!");
+                    Console.WriteLine("You died...");
+                    break;
+                }
+            }
+            if (rooms[index] == "Rest")
+            {
+                Console.WriteLine("Do you wish to recover your hp for the giving trials ahead? (Y/N)");
+            }
+            if (rooms[index] == "Battle")
+            {
+                Console.WriteLine("TIME FOR BATTLE");
+            }
+        }
+    }
+}
--- obj/Debug/net10.0/a.c..AssemblyInfo.cs
+++ obj/Debug/net10.0/a.c..AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("a.c.")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ec9a2cb58b242d4c45cd7cbd100bc6656a445e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("a.c.")]
 [assembly: System.Reflection.AssemblyTitleAttribute("a.c.")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
